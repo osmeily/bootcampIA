04f59031da3aae60d3a447cab79dbb5dc03fd82d
osmeily <osmeilypernia@gmail.com>
2022-12-10 12:07:22 -0500

    init files, folder structure & some assets

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors: {
+      darkBlue: "#162644",
+    },
+    extend: {},
+  },
+  plugins: [],
+}
