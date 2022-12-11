f9d2dc34a96edfee2a45fb24be3fff7948bfa279
osmeily <osmeilypernia@gmail.com>
2022-12-10 23:19:54 -0500

    feature: steps & button styling

---
+++ src/templateScripts/step.js
@@ -0,0 +1,63 @@
+const stepTemplate = document.getElementById('step-template').content
+const stepContainer = document.getElementById('steps-container')
+
+const steps = [
+    {
+        id: 1,
+        title: '01',
+        content: 'Primero debes conectar con nosotros para que podamos conocerte y saber que necesitas.',
+        border: 'border-darkPurple',
+        background: 'bg-darkPurple',
+        src: '../src/assets/icono4.png',
+    },
+    {
+        id: 2,
+        title: '02',
+        content: 'Accede a nuestro formulario de contacto, diligéncialo y envíanos tus datos.',
+        border: 'border-grayBlue',
+        background: 'bg-grayBlue',
+        src: '../src/assets/icono3.png'
+    },
+    {
+        id: 3,
+        title: '03',
+        content: 'Mantente atento a tu celular, te llegarán mensajes de texto con información importante.',
+        border: 'border-amber-500',
+        background: 'bg-amber-500',
+        src: '../src/assets/icono2.png'
+    },
+    {
+        id: 4,
+        title: '04',
+        content: '¡Bienvenido a la familia!, comparte la noticia con tus amigos.',
+        border: 'border-purple-600',
+        background: 'bg-purple-600',
+        src: '../src/assets/icono1.png'
+    }
+]
+
+document.addEventListener('DOMContentLoaded', () => {
+    setSteps()
+})
+
+const setSteps = () => {
+    steps.forEach(step => {
+        const stepClone = stepTemplate.cloneNode(true)
+        const stepTitle = stepClone.querySelector('#step-title')
+        const stepContent = stepClone.querySelector('#step-content')
+        const stepCircle = stepClone.querySelector('#step-circle')
+        const stepIcon = stepClone.querySelector('#step-icon')
+        const topLine = stepClone.querySelector('#top-line')
+        const bottomLine = stepClone.querySelector('#bottom-line')
+
+        stepTitle.textContent = step.title
+        stepTitle.classList.add(step.background)
+        stepContent.textContent = step.content
+        stepCircle.classList.add(step.border)
+        stepIcon.src = step.src
+        topLine.classList.add(step.background)
+        bottomLine.classList.add(step.background)
+
+        stepContainer.appendChild(stepClone)
+    })
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}", "./public/index.html"],
+  content: ["./src/**/*.{html,js}", "./public/index.html", "./src/templateScripts/*.js"],
   theme: {
     extend: {
       colors: {
         darkBlue: "#162644",
+        darkPurple: "#430751",
+        grayBlue: "#97A2C9",
+        darkLilac: "#63056D",
       },
       minWidth: {
         m150: "150px",
+        m250: "250px",
+      },
+      height: {
+        120: "120px",
+      },
+      screens: {
+        sm: "500px",
+        preLg: "900px"
       },
     },
   },
