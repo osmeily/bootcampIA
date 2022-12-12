fcc85091a9cdf06d3f96baceb0afe716556d7cac
osmeily <osmeilypernia@gmail.com>
2022-12-12 02:15:29 -0500

    fix: page details

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ const menuIcon = document.querySelector('#menu-icon')
 
 const questionsbtn = document.querySelector('#questions-btn')
 const questions = document.querySelector('#questions')
+const frecuentQuestions = document.querySelectorAll('.preguntas')
+
+const toTopBtn = document.querySelector('#to-top')
 
 menuIcon.addEventListener('click', () => {
     burgerMenu.classList.toggle('flex')
@@ -12,4 +15,15 @@ questionsbtn.addEventListener('click', () => {
     questions.classList.toggle('flex')
     questions.classList.toggle('hidden')}
 )
+
+frecuentQuestions.forEach((question) => {
+    question.addEventListener('click', () => {
+        questions.classList.toggle('flex')
+        questions.classList.toggle('hidden')}
+    )
+})
+
+toTopBtn.addEventListener('click', () => {
+    window.scrollTo(0, 0)
+})
 
